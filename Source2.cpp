199bc702abe144ba24063a902b052db5c09074f9
BuyWell <74830022+BuyWell11@users.noreply.github.com>
2021-02-16 23:52:33 +0300

    Update Source2.cpp

--- Source2.cpp
+++ Source2.cpp
@@ -6,7 +6,7 @@
 #define SIZE 20
 
 typedef struct string_ {
-    char str[SIZE] = {0};
+    char str[SIZE] = { 0 };
 }strings;
 
 typedef struct variable {
@@ -19,39 +19,40 @@ int prior(int symb)
 {
     switch (symb)
     {
+    case'(':
+        return 0;
     case '+':
-        return 1;
-        break;
     case '-':
         return 1;
-        break;
     case'*':
-        return 2;
-        break;
     case'/':
         return 2;
-        break;
+    case'^':
+        return 3;
     }
-    return 0;
+    return 3;
 }
 
-int push(char stack[], unsigned cur, char symb)
+
+
+int push(strings stack[], unsigned cur, char buff[])
 {
     if (cur < SIZE)
     {
-        stack[cur++] = symb;
+        strcpy(stack[cur].str, buff);
+        cur++;
     }
     return cur;
 }
 
-int empty(const char stack[], unsigned cur)
+int empty(const strings stack[], unsigned cur)
 {
     if (cur == 0)
         return 1;
     else return 0;
 }
 
-int pop(char stack[], unsigned cur)
+int pop(strings stack[], unsigned cur)
 {
     if (empty(stack, cur) != 1)
     {
@@ -60,111 +61,171 @@ int pop(char stack[], unsigned cur)
     return cur;
 }
 
-void make_polish(variables var[], char str[], int curva) {
-    int len = 0, cur = 0;
-    char symb[SIZE] = { 0 };
-    for (int i = 0; i < strlen(str); i++)
-    {
-        if (str[i] >= '0' && str[i] <= '9')
-        {
-            var[curva].polish.str[len] = str[i];
-            len++;
-        }
-        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
-        {
-            if (var[curva].polish.str[len - 1] != ' ')
-            {
-                var[curva].polish.str[len] = ' ';
-                len++;
-            }
-            if (empty(symb, cur))
-            {
-                cur = push(symb, cur, str[i]);
-            }
-            else
-                if (prior(str[i]) > prior(symb[cur - 1]))
-                    cur = push(symb, cur, str[i]);
-                else if (prior(str[i]) <= prior(symb[cur - 1]))
-                {
-                    while (prior(str[i]) <= prior(symb[cur - 1]) && !(empty(symb, cur)))
-                    {
-                        var[curva].polish.str[len] = symb[cur - 1];
-                        len++;
-                        var[curva].polish.str[len] = ' ';
-                        len++;
-                        cur = pop(symb, cur);
-                    }
-                    cur = push(symb, cur, str[i]);
-                }
-        }
-        if (str[i] == '(')
-        {
-            if (len == 0)
-                cur = push(symb, cur, str[i]);
-            else
-            {
-                if (var[curva].polish.str[len - 1] != ' ')
-                {
-                    var[curva].polish.str[len] = ' ';
-                    len++;
-                }
-                cur = push(symb, cur, str[i]);
-            }
-        }
-        if (str[i] == ')')
-        {
-            var[curva].polish.str[len] = ' ';
-            len++;
-            while (symb[cur - 1] != '(')
-            {
-                var[curva].polish.str[len] = symb[cur - 1];
-                len++;
-                var[curva].polish.str[len] = ' ';
-                len++;
-                cur = pop(symb, cur);
-            }
-            cur = pop(symb, cur);
-        }
-        if (str[i + 1] == '\0' || str[i + 1] == '\n')
-        {
-            if (var[curva].polish.str[len - 1] != ' ')
-            {
-                var[curva].polish.str[len] = ' ';
-                len++;
-            }
+int is_fun(strings fun[], char buff[], int cur_fun) {
+    for (int i = 0; i < cur_fun; i++) {
+        if (strcmp(fun[i].str, buff) == 0) {
+            return 1;
         }
     }
-    while (!(empty(symb, cur)))
-    {
-        var[curva].polish.str[len] = symb[cur - 1];
-        len++;
-        if (var[curva].polish.str[len - 1] != ' ')
-        {
-            var[curva].polish.str[len] = ' ';
-            len++;
+    return 0;
+}
+
+int is_const(strings con[], char buff[], int cur_con) {
+    for (int i = 0; i < cur_con; i++) {
+        if (strcmp(con[i].str, buff) == 0) {
+            return 1;
         }
-        cur = pop(symb, cur);
+    }
+    return 0;
+}
+
+void buff_clear(char buff[]) {
+    int len = strlen(buff);
+    for (int i = 0; i < len; i++) {
+        buff[i] = '\0';
     }
 }
 
-int main()
-{
-    variables var[SIZE];
-    char str[SIZE] = { 0 };
-    char polish[SIZE] = { 0 };
-    char symb[SIZE] = { 0 };
-    char chif[SIZE] = { 0 };
-    int res[SIZE] = { 0 };
-    int curva = 0;
-    while (true) {
-        scanf("%s = %s", var[curva].name, &var[curva].original.str);
-        if (strcmp(var[curva].name, "stop") == 0) {
+void make_polish(variables var[], char str[], int curva, strings fun[], strings con[], int cur_fun, int cur_con, strings stack[], FILE *input) {
+    int len = 0, cur = 0;
+    char buff[SIZE] = { 0 };
+    char c = '0';
+    char check[SIZE] = { 0 };
+    while (c != '\0') {
+        fscanf(input, "%s", buff);
+        if (strcmp(buff, check) == 0) {
             break;
         }
+        if (is_fun(fun, buff, cur_fun) == 1) {
+            if (empty(stack, cur) == 1) {
+               cur = push(stack, cur, buff);
+            }
+            else if (prior(buff[0]) > prior(stack[cur - 1].str[0])) {
+                cur = push(stack, cur, buff);
+            }
+            else if (prior(buff[0]) <= prior(stack[cur - 1].str[0])) {
+                while (prior(buff[0]) <= prior(stack[cur - 1].str[0]) && !empty(stack, cur)) {
+                    strcat(var[curva].polish.str, stack[cur-1].str);
+                    var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                    cur = pop(stack, cur);
+                }
+                cur = push(stack, cur, buff);
+            }
+        }
+        else if (strcmp(buff, "(") == 0) {
+            cur = push(stack, cur, buff);
+        }
+        else if (strcmp(buff, ")") == 0) {
+            var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+            while (strcmp(stack[cur - 1].str, ")") != 0) {
+                strcat(var[curva].polish.str, stack[cur - 1].str);
+                var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                cur = pop(stack, cur);
+            }
+            cur = pop(stack, cur);
+        }
         else {
-            make_polish(var, var[curva].original.str, curva);
+           cur = push(stack, cur, buff);
+        }
+        strcpy(check, buff);
+    }
+    if (empty(stack, cur) != 1) {
+        while (empty(stack, cur) != 1) {
+            strcat(var[curva].polish.str, stack[cur - 1].str);
+            var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+            cur = pop(stack, cur);
         }
     }
+}
+
+int main(){
+    variables var[SIZE];
+    FILE *fun = fopen("function.txt", "r");
+    FILE *con = fopen("const.txt", "r");
+    FILE *input = fopen("input.txt", "r");
+    strings fun_arr[SIZE];
+    strings con_arr[SIZE];
+    strings stack[SIZE];
+    char str[SIZE] = { 0 };
+    char c = '0';
+    int curva = 0, cur_fun = 0, cur_con = 0;
+    while (!feof(fun)) {
+        fscanf(fun,"%s", fun_arr[cur_fun].str);
+        cur_fun++;
+    }
+    while (!feof(con)) {
+        fscanf(con,"%s", con_arr[cur_con].str);
+        cur_con++;
+    }
+    while (!feof(input)) {
+        fscanf(input, "%s", var[curva].name);
+        c = fgetc(input);
+        c = fgetc(input);
+        c = fgetc(input);
+        int len = 0, cur = 0;
+        char buff[SIZE] = { 0 };
+        char check[SIZE] = { 0 };
+        while (c != '\n' && c != EOF) {
+            fscanf(input, "%s", buff);
+            if (is_fun(fun_arr, buff, cur_fun) == 1) {
+                if (empty(stack, cur) == 1) {
+                    cur = push(stack, cur, buff);
+                }
+                else if (prior(buff[0]) > prior(stack[cur - 1].str[0])) {
+                    cur = push(stack, cur, buff);
+                }
+                else if (prior(buff[0]) <= prior(stack[cur - 1].str[0])) {
+                    while (prior(buff[0]) <= prior(stack[cur - 1].str[0]) && !empty(stack, cur)) {
+                        strcat(var[curva].polish.str, stack[cur - 1].str);
+                        var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                        cur = pop(stack, cur);
+                    }
+                    cur = push(stack, cur, buff);
+                }
+            }
+            else if (strcmp(buff, "(") == 0) {
+                cur = push(stack, cur, buff);
+            }
+            else if (strcmp(buff, ")") == 0) {
+                while (strcmp(stack[cur - 1].str, "(") != 0) {
+                    strcat(var[curva].polish.str, stack[cur - 1].str);
+                    var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                    cur = pop(stack, cur);
+                }
+                cur = pop(stack, cur);
+            }
+            else if(strcmp(buff,"+") == 0 || strcmp(buff, "-") == 0 || strcmp(buff, "*") == 0 || strcmp(buff, "/") == 0 || strcmp(buff, "^") == 0) {
+                if (empty(stack, cur) == 1) {
+                    cur = push(stack, cur, buff);
+                }
+                else if (prior(buff[0]) > prior(stack[cur - 1].str[0])) {
+                    cur = push(stack, cur, buff);
+                }
+                else if (prior(buff[0]) <= prior(stack[cur - 1].str[0])) {
+                    while (prior(buff[0]) <= prior(stack[cur - 1].str[0]) && !empty(stack, cur)) {
+                        strcat(var[curva].polish.str, stack[cur - 1].str);
+                        var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                        cur = pop(stack, cur);
+                    }
+                    cur = push(stack, cur, buff);
+                }
+            }
+            else {
+                strcat(var[curva].polish.str, buff);
+                var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+            }
+            c = fgetc(input);
+        }
+        if (empty(stack, cur) != 1) {
+            while (empty(stack, cur) != 1) {
+                strcat(var[curva].polish.str, stack[cur - 1].str);
+                var[curva].polish.str[strlen(var[curva].polish.str)] = ' ';
+                cur = pop(stack, cur);
+            }
+        }
+        curva++;
+    }
+    printf("%s", var[0].polish.str);
     /* for (int i = 0; i < strlen(polish) - 1; i++)
      {
          if (polish[i] >= '0' && polish[i] <= '9')
@@ -207,6 +268,7 @@ int main()
          len = 0;
          cur++;
      }*/
-    printf("Result:\n");
-    printf("%d", res[0]);
+    fclose(fun);
+    fclose(con);
+    fclose(input);
 }
